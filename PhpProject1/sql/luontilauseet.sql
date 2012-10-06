70a8dbe677b1bf4227d98fed7cb60f7c99c2eb2e
Morrow3 <joni.hauhia@cs.helsinki.fi>
2012-10-06 21:32:19 +0300

    new file:   sql/luontilauseet.sql

---
+++ PhpProject1/sql/luontilauseet.sql
@@ -0,0 +1,20 @@
+--Käyttäjätaulu
+CREATE TABLE kayttajat (
+id serial PRIMARY KEY,
+enimi varchar,
+snimi varchar,
+ssana varchar,
+sposti varchar UNIQUE,
+on_admin boolean DEFAULT FALSE
+);
+
+--Juomataulu
+CREATE TABLE juomat (
+id serial PRIMARY KEY,
+kayttaja_id integer REFERENCES kayttajat(id) ON DELETE CASCADE,
+juoman_maara integer,
+milloin_juotu timestamp,
+missa_juotu varchar,
+mita_juotu varchar,
+juoman_hinta decimal,
+);
