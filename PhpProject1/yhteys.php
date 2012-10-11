3e7307a0241d30038384d71a987c96f2a8d8c001
Morrow3 <joni.hauhia@cs.helsinki.fi>
2012-10-11 16:47:13 +0300

    ja uusiks..

--- PhpProject1/yhteys.php
+++ PhpProject1/yhteys.php
@@ -5,19 +5,10 @@ $password = "571f9d229df5a03f";
 
 try
 {
-<<<<<<< HEAD
     $pdo = new PDO("pgsql:host=localhost;dbname=$database", "$user", "$password");
 } 
 catch (PDOException $e) 
 {
     die("VIRHE! " . $e->getMessage());
-=======
-    $pdo = new PDO("pgsql:host=localhost; dbname=jhauhia", 
-            "jhauhia", "xxxx");    
-<<<<<<< HEAD
->>>>>>> branch 'master' of https://github.com/Morrow3/FuksinLaskuri.git
-=======
->>>>>>> e7a00e73aefb880d79a4a3792e0aeacd662bb348
 }
-//$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
